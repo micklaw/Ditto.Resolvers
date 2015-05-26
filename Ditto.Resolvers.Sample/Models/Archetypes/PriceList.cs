01a6e60d2c0826a507cc8ad191f5e84fe66f35bc
michaellaw <michael.law@sky.com>
2015-05-26 22:42:54 +0100

    adding sample solution

---
+++ Ditto.Resolvers.Sample/Models/Archetypes/PriceList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using Archetype.Models;
+using Ditto.Resolvers.Sample.Models.DocTypes.Base;
+using Our.Umbraco.Ditto;
+
+namespace Ditto.Resolvers.Sample.Models.Archetypes
+{
+    public class PriceList : ArchetypeFieldsetModel
+    {
+        public string Title { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Price { get; set; }
+
+        [TypeConverter(typeof(DittoContentPickerConverter))]
+        public Content AssociatedPage { get; set; }
+    }
+}
---
+++ Ditto.Resolvers.Sample/Models/DocTypes/TextPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ditto.Resolvers.Sample.Models.Archetypes;
+using Ditto.Resolvers.Sample.Models.DocTypes.Base;
+using Our.Umbraco.Ditto.Resolvers.Archetype.Attributes;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Ditto.Resolvers.Sample.Models.DocTypes
+{
+    public class TextPage : Page
+    {
+        public TextPage(IPublishedContent content) : base(content)
+        {
+        }
+
+        public string Title { get; set; }
+
+        public HtmlString Body { get; set; }
+    }
+}
