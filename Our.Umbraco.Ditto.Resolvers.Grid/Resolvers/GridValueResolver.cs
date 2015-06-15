b1c67faa29e7387153b4aba4053aaf83dc51033c
jamiepollock <jamie.does.umbraco@gmail.com>
2015-06-15 16:00:56 +0100

    Initial commit of Grid Value Resolver. Far from the finished initigrated product, just testing the waters with this commit.

---
+++ Our.Umbraco.Ditto.Resolvers.Grid/Resolvers/GridValueResolver.cs
@@ -0,0 +1,28 @@
+﻿using Our.Umbraco.Ditto.Resolvers.Grid.Attributes;
+using System.ComponentModel;
+using System.Globalization;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace Our.Umbraco.Ditto.Resolvers.Grid.Resolvers {
+    public class GridValueResolver : DittoValueResolver<GridResolverAttribute> {
+        public override object ResolveValue(ITypeDescriptorContext context, GridResolverAttribute attribute, CultureInfo culture) {
+            var content = context.Instance as IPublishedContent;
+            var descriptor = context.PropertyDescriptor;
+
+            if (content != null && descriptor != null) {
+                var alias = attribute.PropertyAlias ?? descriptor.DisplayName;
+
+                if (content.HasValue(alias)) {
+                    var output = attribute.HasFramework
+                        ? content.GetGridHtml(alias, attribute.Framework)
+                        : content.GetGridHtml(alias);
+
+                    return output;
+                }
+            }
+
+            return null;
+        }
+    }
+}
