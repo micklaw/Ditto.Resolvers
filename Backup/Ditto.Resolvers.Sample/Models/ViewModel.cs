50414f845ab6dfbd5815e65fdd5b4ee60f0f26a1
Michael Law <michael@michaellaw.me>
2016-01-02 09:26:17 +0000

    amending build process

--- Backup/Ditto.Resolvers.Sample/Models/ViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ditto.Resolvers.Sample.Models.DocTypes.Base;
-
-namespace Ditto.Resolvers.Sample.Models
-{
-    public class ViewModel<T> where T : Content
-    {
-        public T Content { get; set; }
-    }
-}
