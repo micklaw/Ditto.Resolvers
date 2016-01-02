ae02ad8673bdbf1d8026f0a11c7ee9b099085fd9
Michael Law <michael@michaellaw.me>
2016-01-02 10:25:38 +0000

    bumping version for deploy

--- Ditto.Resolvers.Sample/Properties/AssemblyInfo.cs
+++ Ditto.Resolvers.Sample/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ditto.Resolvers.Sample")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ditto.Resolvers.Sample")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitleAttribute("Ditto.Resolvers.Sample")]
+[assembly: AssemblyDescriptionAttribute("")]
+[assembly: AssemblyConfigurationAttribute("")]
+[assembly: AssemblyCompanyAttribute("MLWD")]
+[assembly: AssemblyProductAttribute("Ditto.Resolvers.Sample")]
+[assembly: AssemblyCopyrightAttribute("Copyright (c) MLWD 2015")]
+[assembly: AssemblyTrademarkAttribute("")]
+[assembly: AssemblyCultureAttribute("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
