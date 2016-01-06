f399f09c810359cc73071f2d6e55950a6084259f
Michael Law <michael@michaellaw.me>
2016-01-06 22:39:28 +0000

    amending build 1.0.2 as i forgot to version assemblies

--- Our.Umbraco.Ditto.Resolvers.Archetype/Properties/AssemblyInfo.cs
+++ Our.Umbraco.Ditto.Resolvers.Archetype/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("1.0.1")]
-[assembly: AssemblyFileVersionAttribute("1.0.1")]
+[assembly: AssemblyVersionAttribute("1.0.2")]
+[assembly: AssemblyFileVersionAttribute("1.0.2")]
 
--- Our.Umbraco.Ditto.Resolvers/Properties/AssemblyInfo.cs
+++ Our.Umbraco.Ditto.Resolvers/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("1.0.1")]
-[assembly: AssemblyFileVersionAttribute("1.0.1")]
+[assembly: AssemblyVersionAttribute("1.0.2")]
+[assembly: AssemblyFileVersionAttribute("1.0.2")]
 
